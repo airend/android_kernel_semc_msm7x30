21deb9007966fb26c57a5b37eda685bf70ef6b23
D. Andrei Măceș <dmaces@nd.edu>
2017-03-13 14:48:36 -0400

    mogami: wifi: Update to use frequency instead of enumerations
    Change-Id: I1cbbb6407c5aca859c99b211872102a2e761d21b
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- arch/arm/mach-msm/board-semc_mogami-wifi.c
+++ arch/arm/mach-msm/board-semc_mogami-wifi.c
@@ -47,7 +47,7 @@ int mogami_wifi_power(int on)
 struct wl12xx_platform_data mogami_wlan_data __initdata = {
 	.irq = MSM_GPIO_TO_INT(MOGAMI_WIFI_IRQ_GPIO),
 	.irq_trigger = IRQF_TRIGGER_RISING,
-	.board_ref_clock = WL12XX_REFCLOCK_19,
+	.ref_clock_freq = 19200000,
 };
 
 static int __init mogami_wifi_init(void)
